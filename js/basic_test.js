f0e04b62257fd8bcd7906a45dd539af4af69caa5
이준광 <jimmy010011@gmail.com>
2023-04-05 05:32:48 +0000

    ##2023년 4월 5일 5주차 홈페이지 수정 완료

---
+++ js/basic_test.js
@@ -0,0 +1,24 @@
+//한줄 주석 : 임시 자바 스크립트 파일을 생성했습니다
+var jb = 'hi'; // 변수 선언 뒤에 주석처리 가능
+
+/*
+여러줄 주석: 여러줄을 주석 처리 합니다
+*/
+var a =1;
+var b;
+b=5;
+
+if (true){
+	let c ='let 접근';
+	let c_1 = 'var 접근';
+}
+//console.log(c);//Error?
+console.log(c_1);
+let d = 5;
+//let d = '값 재할당';// Error?
+console.log(d);
+
+const e = '상수1 접근';
+//e = 5;
+//const f //Error?
+console.log(e);
